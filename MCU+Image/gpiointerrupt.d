c5c2619552157a4ed3a134a5b2f81c52652b3a3b
Nick Galantino <nick.galantino@gmail.com>
2023-11-25 10:24:40 -0500

    Build project

---
+++ MCU+Image/gpiointerrupt.d
@@ -0,0 +1,24 @@
+# FIXED
+
+gpiointerrupt.o: ../gpiointerrupt.c \
+ C:/ti/simplelink_cc32xx_sdk_7_10_00_13/source/ti/drivers/GPIO.h \
+ C:/ti/simplelink_cc32xx_sdk_7_10_00_13/source/ti/devices/DeviceFamily.h \
+ C:/ti/simplelink_cc32xx_sdk_7_10_00_13/source/ti/drivers/gpio/GPIOCC32XX.h \
+ C:/ti/simplelink_cc32xx_sdk_7_10_00_13/source/ti/devices/cc32xx/inc/hw_types.h \
+ C:/ti/simplelink_cc32xx_sdk_7_10_00_13/source/ti/devices/cc32xx/driverlib/gpio.h \
+ C:/ti/simplelink_cc32xx_sdk_7_10_00_13/source/ti/devices/cc32xx/driverlib/pin.h \
+ syscfg/ti_drivers_config.h
+
+C:/ti/simplelink_cc32xx_sdk_7_10_00_13/source/ti/drivers/GPIO.h:
+
+C:/ti/simplelink_cc32xx_sdk_7_10_00_13/source/ti/devices/DeviceFamily.h:
+
+C:/ti/simplelink_cc32xx_sdk_7_10_00_13/source/ti/drivers/gpio/GPIOCC32XX.h:
+
+C:/ti/simplelink_cc32xx_sdk_7_10_00_13/source/ti/devices/cc32xx/inc/hw_types.h:
+
+C:/ti/simplelink_cc32xx_sdk_7_10_00_13/source/ti/devices/cc32xx/driverlib/gpio.h:
+
+C:/ti/simplelink_cc32xx_sdk_7_10_00_13/source/ti/devices/cc32xx/driverlib/pin.h:
+
+syscfg/ti_drivers_config.h:
